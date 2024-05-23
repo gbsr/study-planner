b2a0310525fb45ebb7466b1932b0057ed283a9cc
Anders Hofsten <anders.jakobsen.hofsten@gmail.com>
2024-05-24 01:35:06 +0200

    Render items in footer to let you know how much is done

--- src/components/Footer.cy.jsx
+++ src/components/Footer.cy.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
-import Footer from './Footer'
+import Footer from "./Footer";
 
-describe('<Footer />', () => {
-  it('renders', () => {
-    // see: https://on.cypress.io/mounting-react
-    cy.mount(<Footer />)
-  })
-})
+describe("<Footer />", () => {
+	it("renders", () => {
+		cy.mount(<Footer />);
+	});
+
+	it("should render the current done items and the current total items", () => {
+		const todos = [
+			{ id: 1, done: true },
+			{ id: 2, done: false },
+			{ id: 3, done: true },
+		];
+
+		cy.mount(<Footer todos={todos} />);
+		cy.get("footer").contains("Du har klarat av 2 av 3 saker på din lista.");
+	});
+});
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,16 +1,27 @@
-import { getToday } from "../utils/date";
-import { getCurrentDate } from "../utils/date";
-<utils />;
+import { getToday, getCurrentDate } from "../utils/date";
+import { useStore } from "../data/store";
 
-const currentDay = getToday();
-const currentDate = getCurrentDate();
-const Footer = () => (
-	<footer>
-		<p>
-			Idag är det: <b>{currentDay}</b>, <b>{currentDate}</b>
-		</p>
-		<p> Studieguide | 2024 </p>
-	</footer>
-);
+const Footer = ({ todos: todosProp }) => {
+	const todosFromStore = useStore((state) => state.todos);
+	let todos = todosProp || todosFromStore;
+
+	const doneItems = todos.filter((todo) => todo.done).length;
+	const allItems = todos.length;
+
+	const currentDay = getToday();
+	const currentDate = getCurrentDate();
+
+	return (
+		<footer>
+			<div>
+				Idag är det: <b>{currentDay}</b>, <b>{currentDate}</b>
+				<p>
+					Du har klarat av <b>{doneItems}</b> av <b>{allItems}</b> saker på din lista.
+				</p>
+			</div>
+			<p> Studieguide | 2024 </p>
+		</footer>
+	);
+};
 
 export default Footer;
