b01f7effc67964365a0d2c5526f939e4261f6f4e
Anders Hofsten <anders.jakobsen.hofsten@gmail.com>
2024-05-26 13:28:44 +0200

    Forgot to test - restart week. Yes, clicking that button actually works. yay.

--- src/components/Header.cy.jsx
+++ src/components/Header.cy.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
-import Header from './Header'
+import Header from "./Header";
 
-describe('<Header />', () => {
-  it('renders', () => {
-    // see: https://on.cypress.io/mounting-react
-    cy.mount(<Header />)
-  })
-})
+describe("<Header />", () => {
+	it("renders", () => {
+		// see: https://on.cypress.io/mounting-react
+		cy.mount(<Header />);
+	});
+});
+
+describe("It restarts the week", () => {
+	it("should restart the week", () => {
+		cy.mount(<Header />);
+		cy.get(".restart-week").click();
+	});
+});
